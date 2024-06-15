6aa67648d3ce7649116655f95ac9fe3302abf011
Jessica Bernardo <jessicaaurorabernardo@gmail.com>
2024-06-15 12:17:05 -0300

    Exercicio semana 3

---
+++ Condicionais/if_exercicio1.py
@@ -0,0 +1,5 @@
+nota = float(input("Insira a nota da aluna (de 1 a 10): "))
+if nota >= 7
+    print("A aluna foi aprovada.")
+else:
+    print("A aluna foi reprovada.")
---
+++ Logico/if_exercicio2.py
@@ -0,0 +1,7 @@
+nota = float(input("Insira a nota da aluna (de 1 a 10): "))
+frequencia = float(input("Insira a frequencia da aluna 0 100: "))
+if nota >= 7 and frequencia >= 75:
+    print("A aluna foi aprovada.")
+else:
+    print("A aluna foi reprovada.")
+
